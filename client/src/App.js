82c9b4fcc6f04a39cec288a46bdad24c180b744f
MIchael Newman <mikeronewman@gmail.com>
2021-10-09 18:57:09 -0400

    Removed boilerplate and added css baseline

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} >
+        <Stack direction="column" spacing={2}>
+          <h1>Hello Planning Poker!</h1>
+          <Button variant="contained">Contained</Button>
+          <Button variant="contained" disabled>
+            Disabled
+          </Button>
+          <Button variant="contained" href="#contained-buttons">
+            Link
+          </Button>
+        </Stack>
+        </Box>
+      </Container>
+    
   );
 }
-
-export default App;
